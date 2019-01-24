1b845e145cdcf33a39af430d0cbb5ff973a7c43e
gabrielfellon <gabrielfellone@hotmail.com>
2019-01-24 17:02:25 -0200

    Zabbix API Exemple

--- ZabbixAPI.py
+++ ZabbixAPI.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 #-*-coding: utf-8-*-
 from zabbix_api import ZabbixAPI
-zapi=ZabbixAPI(server="http://zabbix.em2it.com/zabbix")
-zapi.login("evandro.bagini","Sistemas2112")
+zapi=ZabbixAPI(server="SITE")
+#Conexão com Zabbix Server
+zapi.login("Login","Senha")
+#Versao do Zabbix 
 print "Version Zabbix:", zapi.api_version()
+#Printa os hosts 
 hosts = zapi.host.get({"output": "extend", "sortfield": "name"})
 for x in hosts:
     print x['available'], "-", x['hostid'], "-", x['name']
-uptime = zapi.item.get({'output': "extend", 'hostids': 10262,'search': {'key_':'system.uptime'}})
+#Printa os Triggers
+trigger = zapi.trigger.get(only_true=1,
+                            skipDependent=1,
+                            monitored=1,
+                            active=1,
+                            output='extend',
+                            expandDescription=1,
+                            selectHosts=['host'],
+                            )
+
+
+for y in trigger:
+	print y['description'], "--", y['host']
+#Print UPTIME
+uptime = zapi.item.get({'output': "extend", 'hostids': 10084, 'search': {'key_':'system.uptime'}})
 print "Tempo",uptime[0]['lastvalue']
+trigger = zapi.trigger.get[{'output': "extend", 'group': "HSM"}]
+print "Triggers",trigger
